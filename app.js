36c23de6f233ccdc19aa68c2d8a9d672dd1ec029
dee2018 <deekurmi2018@gmail.com>
2021-05-21 02:11:59 +0530

    4th commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+dotenv.config({path: './config.env'});
+
+require('./db/conn');
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
      res.send("this is home page");
@@ -13,6 +21,6 @@ app.get("/signup", (req, res) => {
     res.send("this is signup page");
 });
 
-app.listen(3000 , function(){
-    console.log("server is started");
+app.listen(PORT , function(){
+    console.log('server is started '+ PORT);
 });
