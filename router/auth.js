92c0cff6e4cd238f679b94699b6a5a2ebf7bedb8
dee2018 <deekurmi2018@gmail.com>
2021-05-24 21:36:40 +0530

    7th commit

--- router/auth.js
+++ router/auth.js
@@ -10,7 +10,21 @@ router.get("/", (req, res) => {
  });
  
  router.post("/register", (req, res) => {
+      const {name , email , password , cpassword} = req.body;
+      if(!name || !email || !password || !cpassword)
+      {
+           return res.status(422).json({error : "plz fill all required field"});
+      }
+      User.findOne({email : email}).then((userExists) => {
+           if(userExists){
+          return res.status(422).json({error : "user already exists"});
+           }
+           const user = new User({name : name, email : email, password : password, cpassword : cpassword});
+      user.save().then((success) => {res.status(201).json({message:"successfully registered"})}).catch((error) => {res.status(500).json({error})});
+
+      }).catch((err)=>{console.log(err)});
+
       
- });
+});
 
 module.exports = router;
