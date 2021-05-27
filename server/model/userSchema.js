7844883b6ecbd6bab5356a4770c3e7cee9e753df
dee2018 <deekurmi2018@gmail.com>
2021-05-28 02:39:48 +0530

    clientSide work is going to start

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,56 +1,59 @@
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
-    name: {
-        type: 'string',
-        required: true
+  name: {
+    type: "string",
+    required: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  cpassword: {
+    type: "string",
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: "string",
+        required: true,
+      },
     },
-    email: {
-        type: 'string',
-        required: true
-    },
-    password: {
-        type: 'string',
-        required: true
-    },
-    cpassword: {
-        type: 'string',
-        required: true
-    },
-    tokens: [{
-        token: {
-            type: 'string',
-            required: true
-        }
-    }]
+  ],
 });
 
-userSchema.pre('save', async function (next) {
-
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 12);
-        this.cpassword = await bcrypt.hash(this.cpassword, 12);
-    }
-    next();
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+  next();
 });
 
 //generating token
 userSchema.methods.generateAuthToken = async function () {
-    try {
-        let token = jwt.sign({
-            _id: this._id
-        }, process.env.SECRET_KEY);
-        this.tokens = this.tokens.concat({
-            token: token
-        });
-        await this.save();
-        return token;
+  try {
+    let token = jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.SECRET_KEY
+    );
+    this.tokens = this.tokens.concat({
+      token: token,
+    });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const User = mongoose.model('USER', userSchema);
+const User = mongoose.model("USER", userSchema);
 module.exports = User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-require('../db/conn');
+require("../db/conn");
 
-const User = require('../model/userSchema');
+const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
-     res.send("this is router");
+  res.send("this is router");
 });
 
 //  router.post("/register", (req, res) => {
@@ -26,94 +26,82 @@ router.get("/", (req, res) => {
 
 //       }).catch((err)=>{console.log(err)});
 
-
 // });
 
 router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, cpassword } = req.body;
 
-     try {
-          const {
-               name,
-               email,
-               password,
-               cpassword
-          } = req.body;
+    if (!name || !email || !password || !cpassword) {
+      return res.status(422).json({
+        error: "plz fill all required field",
+      });
+    } else if (password != cpassword) {
+      return res.status(422).json({
+        error: "password and comfirm password are not matched",
+      });
+    }
 
-          if (!name || !email || !password || !cpassword) {
-               return res.status(422).json({
-                    error: "plz fill all required field"
-               });
-          } else if (password != cpassword) {
-               return res.status(422).json({
-                    error: "password and comfirm password are not matched"
-               });
-          }
+    const userExists = await User.findOne({
+      email: email,
+    });
 
-          const userExists = await User.findOne({
-               email: email
-          });
+    if (userExists) {
+      return res.status(422).json({
+        error: "user already exists",
+      });
+    }
+    const user = new User({
+      name: name,
+      email: email,
+      password: password,
+      cpassword: cpassword,
+    });
 
-          if (userExists) {
-               return res.status(422).json({
-                    error: "user already exists"
-               });
-          }
-          const user = new User({
-               name: name,
-               email: email,
-               password: password,
-               cpassword: cpassword
-          });
-
-          await user.save();
-          res.status(201).json({
-               message: "you have successfully registered!!"
-          });
-     } catch (err) {
-          console.log(err)
-     }
-
+    await user.save();
+    res.status(201).json({
+      message: "you have successfully registered!!",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-     try {
-          const {
-               email,
-               password,
-          } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-          if (!email || !password) {
-               return res.status(422).json({
-                    error: "plz fill all required field"
-               });
-          }
-          const userData = await User.findOne({
-               email: email
-          });
-          if (!userData) {
-               return res.status(422).json({
-                    error: "email or password is incorrect"
-               });
-          }
-          const token = await userData.generateAuthToken();
-          console.log(token);
-          res.cookie("userToken", token, {
-               expires: new Date(Date.now() + 60000000),
-               httpOnly: true
-          });
-          const isMatched = await bcrypt.compare(password, userData.password);
-          if (isMatched) {
-               return res.status(200).json({
-                    message: "login successful"
-               });
-          } else {
-               return res.status(400).json({
-                    error: "email or password is incorrect"
-               });
-          }
-
-     } catch (error) {
-          console.log(error);
-     }
+    if (!email || !password) {
+      return res.status(422).json({
+        error: "plz fill all required field",
+      });
+    }
+    const userData = await User.findOne({
+      email: email,
+    });
+    if (!userData) {
+      return res.status(422).json({
+        error: "email or password is incorrect",
+      });
+    }
+    const token = await userData.generateAuthToken();
+    console.log(token);
+    res.cookie("userToken", token, {
+      expires: new Date(Date.now() + 60000000),
+      httpOnly: true,
+    });
+    const isMatched = await bcrypt.compare(password, userData.password);
+    if (isMatched) {
+      return res.status(200).json({
+        message: "login successful",
+      });
+    } else {
+      return res.status(400).json({
+        error: "email or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 module.exports = router;
